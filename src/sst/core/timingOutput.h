b43f62119ecdb2f918f73d20ff9424b2b82db36f
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-04-17 14:31:59 -0700

    working json output for timing info

--- src/sst/core/timingOutput.h
+++ src/sst/core/timingOutput.h
@@ -29,29 +29,51 @@ public:
     /** Timing Parameters
      */
     enum Key {
-        LOCAL_MAX_RSS,
-        GLOBAL_MAX_RSS,
-        LOCAL_MAX_PF,
-        GLOBAL_PF,
-        GLOBAL_MAX_IO_IN,
-        GLOBAL_MAX_IO_OUT,
-        GLOBAL_MAX_SYNC_DATA_SIZE,
-        GLOBAL_SYNC_DATA_SIZE,
-        MAX_MEMPOOL_SIZE,
-        GLOBAL_MEMPOOL_SIZE,
-        MAX_BUILD_TIME,
-        MAX_RUN_TIME,
-        MAX_TOTAL_TIME,
-        SIMULATED_TIME_UA,
-        GLOBAL_ACTIVE_ACTIVITIES,
-        GLOBAL_CURRENT_TV_DEPTH,
-        GLOBAL_MAX_TV_DEPTH,
+        LOCAL_MAX_RSS,              // Max Resident Set Size (kB)
+        GLOBAL_MAX_RSS,             // Approx. Global Max RSS Size (kB)
+        LOCAL_MAX_PF,               // Max Local Page Faults
+        GLOBAL_PF,                  // Global Page Faults
+        GLOBAL_MAX_IO_IN,           // Max Output Blocks
+        GLOBAL_MAX_IO_OUT,          // Max Input Blocks
+        GLOBAL_MAX_SYNC_DATA_SIZE,  // Max Sync data size
+        GLOBAL_SYNC_DATA_SIZE,      // Global Sync data size
+        MAX_MEMPOOL_SIZE,           // Max mempool usage (bytes)
+        GLOBAL_MEMPOOL_SIZE,        // Global mempool usage (bytes)
+        MAX_BUILD_TIME,             // Build time (wallclock seconds)    
+        MAX_RUN_TIME,               // Run loop time (wallclock seconds)
+        MAX_TOTAL_TIME,             // Total time (wallclock seconds)
+        SIMULATED_TIME_UA,          // Simulated time ( Algebra seconds string. eg. "10 us" )
+        GLOBAL_ACTIVE_ACTIVITIES,   // Global active activities
+        GLOBAL_CURRENT_TV_DEPTH,    // Current global TimeVortex depth
+        GLOBAL_MAX_TV_DEPTH,        // Max TimeVortex depth
+    };
+
+    const std::map<Key, const char*> key2cstr = {
+        {LOCAL_MAX_RSS, "local_max_rss" },
+        {GLOBAL_MAX_RSS, "global_max_rss"},
+        {LOCAL_MAX_PF, "local_max_pf"},
+        {GLOBAL_PF, "global_pf"},
+        {GLOBAL_MAX_IO_IN, "global_max_io_in"},
+        {GLOBAL_MAX_IO_OUT, "global_max_io_out"},
+        {GLOBAL_MAX_SYNC_DATA_SIZE, "global_max_sync_data_size"},
+        {GLOBAL_SYNC_DATA_SIZE, "global_sync_data_size"},
+        {MAX_MEMPOOL_SIZE, "max_mempool_size"},
+        {GLOBAL_MEMPOOL_SIZE, "global_mempool_size"},
+        {MAX_BUILD_TIME, "max_build_time"},
+        {MAX_RUN_TIME, "max_run_time"},
+        {MAX_TOTAL_TIME, "max_total_time"},
+        {SIMULATED_TIME_UA, "simulated_time_ua"},
+        {GLOBAL_ACTIVE_ACTIVITIES, "global_active_activities"},
+        {GLOBAL_CURRENT_TV_DEPTH, "global_current_tv_depth"},
+        {GLOBAL_MAX_TV_DEPTH, "global_max_tv_depth"},
     };
 
     TimingOutput(const SST::Output& output, bool printEnable);
     virtual ~TimingOutput();
     void setJSON(const std::string& path);
     void generate();
+    void renderText();
+    void renderJSON();
 
     void set(Key key, uint64_t v);
     void set(Key key, UnitAlgebra v);
@@ -60,6 +82,7 @@ public:
  private:
     SST::Output output_;
     bool printEnable_;
+    bool jsonEnable_;
 
     std::map<Key, uint64_t> u64map_ = {};
     std::map<Key, UnitAlgebra> uamap_ = {};
